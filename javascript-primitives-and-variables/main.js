1e85ab5e3c241eca6a5f0d9ec28cf5daff52bfaf
Lawrence Wang <lwang801118@gmail.com>
2023-05-08 20:28:33 +0000

    Completed javascript-primitives-and-variables exercise.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,15 @@
+let fullName = 'Lawrence Wang'
+let isCool = true
+let totalPets = 0
+console.log('value of fullName: ',  fullName);
+console.log('value of of isCool: ',  isCool);
+console.log('value of totalPets: ',  totalPets);
+console.log('typeof fullName: ', typeof fullName)
+console.log('typeof isCool: ', typeof isCool);
+console.log('typeof totalPets: ', typeof totalPets);
+let unknown = null
+console.log('value of unknown: ', unknown)
+console.log('typeof unknown: ', typeof unknown);
+let never
+console.log('value of never: ', never)
+console.log('typeof never: ', typeof never);
