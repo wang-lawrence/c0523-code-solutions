2a8e92d5927353d0779e70498113c7032ceed36e
Lawrence Wang <lwang801118@gmail.com>
2023-05-08 22:07:09 +0000

    javascript-operators-and-expressions - Lawrence Wang

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+const width = 7
+const height = 4
+const area = width * height
+console.log('value of area: ', area)
+console.log('typeof area: ', typeof area)
+
+const bill = 10
+const payment = 4.5
+const change = bill - payment
+console.log('value of change: ', change)
+console.log('typeof change: ', typeof change)
+
+const quizzes = 100
+const midterm = 80
+const final = 90
+const grade = (quizzes + midterm + final) / 3;
+console.log('value of grade: ', grade)
+console.log('typeof grade: ', typeof grade)
+
+const firstName = 'Lawrence'
+const lastName = 'Wang'
+const fullName = firstName + ' ' + lastName;
+console.log('value of fullName', fullName)
+console.log('typeof fullName', typeof fullName)
+
+const ph = 6
+const isAcidic = ph < 7
+console.log('value of isAcidic: ', isAcidic)
+console.log('typeof isAcidic: ', typeof isAcidic)
+
+const headCount = 5
+const isSparta = headCount === 300
+console.log('value of isSparta: ', isSparta)
+console.log('typeof isSparta: ', typeof isSparta)
+
+let  motto = fullName;
+motto += ' is the GOAT'
+console.log('value of motto: ', motto)
+console.log('typeof motto: ', typeof motto)
