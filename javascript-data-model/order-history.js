10ecb342e037ceaf20e61f6af8163dc18a819c24
Lawrence Wang <lwang801118@gmail.com>
2023-05-09 18:53:34 +0000

    javascript-data-model - Lawrence Wang

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,57 @@
+const orderHistory = [
+{
+  orderPlaced: 'August 4, 2020',
+  totalOrder: 34.00,
+  shipTo: 'JS Masher',
+  orderNum: '114-3941689-8772232',
+  delivered: 'August 8, 2020',
+  itemName: 'JavaScript for impatient programmers',
+  author: 'Dr. Axes Rauschmayer',
+  returnWindowClosed: 'September 7, 2020',
+  itemBeforeTax: 31.55
+},
+{
+  orderPlaced: 'July 19, 2020',
+  totalOrder: 44.53,
+  shipTo: 'JS Masher',
+  orderNum: '113-9984268-1280257',
+  delivered: 'July 20, 2020',
+  itemName: 'The Timeless Way of Building',
+  author: 'Christopher Alexander',
+  returnWindowClosed: 'August 19, 2020',
+  itemBeforeTax: 41.33
+},
+{
+  orderPlaced: 'July 4, 2020',
+  totalOrder: 17.22,
+  shipTo: 'JS Masher',
+  orderNum: '114-2875557-9059409',
+  delivered: 'July 7, 2020',
+  itemName: 'Gamecube Controller Adapter',
+  author:null ,
+  returnWindowClosed: 'August 5, 2020',
+  itemBeforeTax: 15.98
+},
+{
+  orderPlaced: 'July 4, 2020',
+  totalOrder: 138.93,
+  shipTo: 'JS Masher',
+  orderNum: '113-2883177-2648248',
+  delivered: 'July 5, 2020',
+  itemName: 'GameCube Controller - Super Smash Bros. Edition',
+  author:null ,
+  returnWindowClosed: 'August 4, 2020',
+  itemBeforeTax: 94.95
+},
+{
+  orderPlaced: 'July 4, 2020',
+  totalOrder: 138.93,
+  shipTo: 'JS Masher',
+  orderNum: '113-2883177-2648248',
+  delivered: 'July 5, 2020',
+  itemName: 'The Art of Sql',
+  author: 'Stephane Faroult',
+  returnWindowClosed: 'August 4, 2020',
+  itemBeforeTax: 33.99
+}
+];
