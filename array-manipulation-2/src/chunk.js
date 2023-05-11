dc009b0cf7775612f1918db175759fce0fc25c5a
Lawrence Wang <lwang801118@gmail.com>
2023-05-11 22:06:48 +0000

    array-manipulation-2 - Lawrence Wang

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,7 +1,7 @@
 /* exported chunk */
 function chunk(array, size) {
   const newArr = [];
-  for (let i = 0; i < Math.ceil(array.length / size); i++) {
+  for (let i = 0; i < Math.ceil(arra_y.length / size); i++) {
     newArr.push(array.slice(i * size, (i+1) * size))
   }
   return newArr;
