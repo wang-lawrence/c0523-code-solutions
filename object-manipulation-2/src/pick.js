fd861a5023d489b6980216a3339770449d11d171
Lawrence Wang <lwang801118@gmail.com>
2023-05-11 23:33:08 +0000

    object-manipulation-2

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -1,10 +1,1 @@
 /* exported pick */
-function pick(source, keys) {
-  const obj = {};
-  for(const key in source) {
-    if (keys.indexOf(key) !== -1 && source[key] !== undefined) {
-      obj[key] = source[key];
-    }
-  }
-  return obj;
-};
