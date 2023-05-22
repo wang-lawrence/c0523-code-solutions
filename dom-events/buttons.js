fd0ddf3fddd17bfff75b47018a3705d9db49dd43
Lawrence Wang <lwang801118@gmail.com>
2023-05-22 21:29:00 +0000

    dom-events - Lawrence Wang

---
+++ dom-events/buttons.js
@@ -0,0 +1,39 @@
+function handleClick(event) {
+  console.log('button clicked');
+  console.log(event.target);
+}
+
+const $clickButton = document.querySelector('.click-button');
+
+$clickButton.addEventListener("click", handleClick)
+
+
+function handleMouseover(event) {
+  console.log('button hovered');
+  console.log(event);
+  console.log(event.target);
+}
+
+const $hoverButton = document.querySelector('.hover-button');
+
+$hoverButton.addEventListener("mouseover", handleMouseover);
+
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.log(event);
+  console.log(event.target);
+}
+
+const $dblClickButton = document.querySelector('.double-click-button');
+
+$dblClickButton.addEventListener("dblclick", handleDoubleClick);
+
+
+function handleClick2() {
+  console.log('button clicked 2');
+  // console.log(event.target);
+}
+
+// const $clickButton = document.querySelector('.click-button');
+
+$clickButton.addEventListener('click', handleClick2());
