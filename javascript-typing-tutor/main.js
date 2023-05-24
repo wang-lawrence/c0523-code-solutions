851f68affd5e2d16ef816798a5588c5940f1cd5b
Lawrence Wang <lwang801118@gmail.com>
2023-05-24 05:31:26 +0000

    javascript-typing-tutor - Lawrence Wang

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,56 @@
+const $letter = document.querySelectorAll('span');
+const $message = document.querySelector('.message');
+const $resetButton = document.querySelector('.reset-button');
+
+let i = 0;
+let typeCount = 0;
+
+console.log($letter.length)
+
+function checky(event) {
+
+typeCount ++;
+
+if (i < $letter.length){
+
+  if ($letter[i].textContent == event.key) {
+    $letter[i].classList.remove('red');
+    $letter[i].classList.add('green');
+    $letter[i].classList.remove('underline');
+
+    if (i < $letter.length - 1) {
+      $letter[i + 1].classList.add('underline');
+      i++;
+    } else {
+      i++;
+    }
+    } else {
+    $letter[i].classList.add('red');
+  }
+
+  switch (true) {
+    case i / $letter.length < 0.25:
+      $message.textContent = 'Begin typing to get started!';
+      break;
+    case i / $letter.length < 0.5:
+      $message.textContent = 'Picking up steam!';
+      break;
+    case i / $letter.length < 0.75:
+      $message.textContent = 'Halfway there, keep it up!';
+      break;
+    case i / $letter.length < 0.9:
+      $message.textContent = "You're almost there!";
+      break;
+    case i / $letter.length == 1:
+      $message.textContent = 'Well done! Your typing accuracy was ' + Math.round(i / typeCount * 100) + '%.';
+      break;
+  }
+ }
+}
+
+function reload (event) {
+  location.reload();
+}
+
+document.addEventListener('keydown', checky);
+$resetButton.addEventListener('click', reload)
