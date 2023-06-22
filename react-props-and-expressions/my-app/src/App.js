bc5d98792eadbffa2d81285b8f922067a6d9952d
Lawrence Wang <lwang801118@gmail.com>
2023-06-22 23:54:18 +0000

    put custom button component it its own file, removed brackets from the string props argument

--- react-props-and-expressions/my-app/src/App.js
+++ react-props-and-expressions/my-app/src/App.js
@@ -1,20 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import './CustomButton.css';
+import CustomButton from './CustomButton';
 
 function App() {
   return (
     <div className="App">
-      <CustomButton text={'I'} color={'red'} />
-      <CustomButton text={'know'} color={'green'} />
-      <CustomButton text={'React!'} color={'blue'} />
+      <CustomButton text='I' color='red' />
+      <CustomButton text='know' color='green' />
+      <CustomButton text='React!' color='blue' />
     </div>
   );
-}
-
-function CustomButton({text, color}) {
-
-  return <button className = {color}>{text}</button>
 }
 
 export default App;
