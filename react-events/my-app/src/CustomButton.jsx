b79cb753fa6068ae538bb91af000fead1f55f808
Lawrence Wang <lwang801118@gmail.com>
2023-06-23 08:15:59 +0000

    deleted old code

--- react-events/my-app/src/CustomButton.jsx
+++ react-events/my-app/src/CustomButton.jsx
@@ -1,9 +1,5 @@
 export default function CustomButton({text, color, onCustomClick}) {
 
-  // function handleClick() {
-  //   alert('You clicked ' + text);
+  return <button style={{backgroundColor: color}} onClick={e => {onCustomClick(text)}}>{text}</button>
 
-  return <button style={{backgroundColor: color}} onClick={e => {
-
-      onCustomClick(text)}}>{text}</button>
 }
