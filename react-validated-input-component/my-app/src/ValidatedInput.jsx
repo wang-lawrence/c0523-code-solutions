7514e52d96f052e2ab615689181215943c459159
Lawrence Wang <lwang801118@gmail.com>
2023-06-28 21:43:13 +0000

    used state to track password instead of display message and styling marke, also added initalAction state to only display messages and marker once user has interacted with the form

--- react-validated-input-component/my-app/src/ValidatedInput.jsx
+++ react-validated-input-component/my-app/src/ValidatedInput.jsx
@@ -2,37 +2,46 @@ import './styles.css'
 import { useState } from 'react'
 
 export default function ValidatedInput() {
-  const [checkMark, setCheckMark] = useState(false);
-  const [message, setMessage] = useState('');
+  const [password, setPassword] =useState('');
+  const [initialAction, setInitialAction] = useState(false)
+
+  let checkMark = false;
+  let message = '';
+
+  if (password === '') {
+    checkMark = false;
+    message = 'A password is required';
+  } else if (password === '') {
+    checkMark = false;
+    message = 'A password is required';
+  } else if (password.length < 8) {
+    checkMark = false;
+    message = 'Your password is too short';
+  } else if (!/[0-9]/.test(password)) {
+    checkMark = false;
+    message = 'Your password needs to contain at least one number';
+  } else if (!/[A-Z]/.test(password)) {
+    checkMark = false;
+    message = 'Your password needs to contain at least one capital letter';
+  } else if (!/[!@#$%^&*()]/.test(password)) {
+    checkMark = false;
+    message = 'Your password needs to contain at least one special character: !, @, #, $, %, ^, &, *, (, or )';
+  } else {
+    checkMark = true;
+    message = ' ';
+  }
 
   function handleChange(e) {
-    if (e.target.value === '') {
-      setCheckMark(false);
-      setMessage('A password is requried')
-    } else if (e.target.value.length < 8) {
-      setCheckMark(false);
-      setMessage('Your password is too short');
-    } else if (!/[0-9]/.test(e.target.value)) {
-      setCheckMark(false);
-      setMessage('Your password needs to contain at least one number');
-    } else if (!/[A-Z]/.test(e.target.value)) {
-      setCheckMark(false);
-      setMessage('Your password needs to contain at least one capital letter');
-    } else if (!/[!@#$%^&*()]/.test(e.target.value)) {
-      setCheckMark(false);
-      setMessage('Your password needs to contain at least one special character: !, @, #, $, %, ^, &, *, (, or )');
-    } else {
-      setCheckMark(true);
-      setMessage(' ');
-    }
+    setInitialAction(true);
+    setPassword(e.target.value)
   }
 
   return (
    <div className = "form-group">
       <label htmlFor="inputPassword5" className="form-label col-sm-12 ml-2" id="password-label">Password</label>
-      <input type="password" id="inputPassword5" className="form-control w-50" aria-labelledby="passwordHelpBlock" onChange={handleChange} />
-      {message && (checkMark ? <i className="fa-solid fa-check"></i> : <i className="fa-solid fa-xmark"></i>)}
-      <small id="passwordHelpBlock" className="form-text">{message} </small>
+      <input type="password" id="inputPassword5" className="form-control w-50" aria-labelledby="passwordHelpBlock" value={password} onChange={handleChange} />
+      {initialAction && (checkMark ? <i className="fa-solid fa-check"></i> : <i className="fa-solid fa-xmark"></i>)}
+      {initialAction && <small id="passwordHelpBlock" className="form-text">{message}</small>}
    </div>
   )
 }
