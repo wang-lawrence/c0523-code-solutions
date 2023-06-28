163694d8d3667bb286384a80226f3d5fca841e4b
Lawrence Wang <lwang801118@gmail.com>
2023-06-28 06:24:00 +0000

    react-validated-input-component

---
+++ react-validated-input-component/my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import ValidatedInput from './ValidatedInput';
+
+function App() {
+  return (
+    <div className="App">
+      <ValidatedInput/>
+    </div>
+  );
+}
+
+export default App;
