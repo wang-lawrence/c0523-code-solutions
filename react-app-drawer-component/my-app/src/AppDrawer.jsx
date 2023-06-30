dfdc9d6109084fabdea0f1f79b1a4db010c2490c
Lawrence Wang <lwang801118@gmail.com>
2023-06-30 09:05:26 +0000

    react-app-drawer-component - Lawrence Wang

---
+++ react-app-drawer-component/my-app/src/AppDrawer.jsx
@@ -0,0 +1,60 @@
+import { FaBars } from 'react-icons/fa';
+import { useState } from 'react';
+
+/**
+ *
+ * @param {Array} items The list of items to display in the menu bar, each item is a string
+ * @returns {Object}
+ */
+export default function AppDrawer({ items, onClickPage }) {
+  const [toggleMenu, setToggleMenu] = useState(true);
+
+  function handleToggleMenu() {
+    setToggleMenu(!toggleMenu);
+  }
+
+  function handleMenuSelect(text) {
+    setToggleMenu(!toggleMenu);
+    onClickPage(text);
+  }
+
+  return (
+    <div>
+      <HamburgerMenu onOpenMenu={handleToggleMenu} />
+      {toggleMenu && <Overlay onCloseMenu={handleToggleMenu} />}
+      <FullMenu
+        items={items}
+        translateX={toggleMenu ? 'translateX(0)' : 'translateX(-14.5rem)'}
+        onCloseMenu={handleMenuSelect}
+      />
+    </div>
+  );
+}
+
+
+function HamburgerMenu({onOpenMenu}) {
+  return (
+    <div className ='menu-icon' onClick={onOpenMenu}>
+      <FaBars  />
+    </div>
+  );
+}
+
+
+function Overlay({onCloseMenu}) {
+  return <div className="overlay" onClick={onCloseMenu}></div>;
+}
+
+function FullMenu({ items, translateX, onCloseMenu }) {
+  const menuItems = items.map((item) => (
+    <p key={item}>
+      <span onClick={() => onCloseMenu(item)}>{item}</span>
+    </p>
+  ))
+
+  return (
+    <div className="fullMenu" style={{ transform: translateX }}>
+      {menuItems}
+    </div>
+  );
+}
