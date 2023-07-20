53791a4e7f9c51310a9612ce7efc19e07752e674
Lawrence Wang <lwang801118@gmail.com>
2023-07-20 17:43:52 +0000

    api-registration - Lawrence Wang

---
+++ api-registration/client/src/NavBar.js
@@ -0,0 +1,27 @@
+export function NavBar({ onNavigate }) {
+  return (
+    <header className="header purple-background">
+      <div className="container">
+        <div className="row">
+          <div className="column-full d-flex align-center">
+            <h1 className="white-text">Code Journal</h1>
+            <h3>
+              <button
+                type="button"
+                onClick={() => onNavigate('journal')}
+                className="entries-link white-text">
+                Entries
+              </button>
+              <button
+                type="button"
+                onClick={() => onNavigate('register')}
+                className="entries-link white-text">
+                Register
+              </button>
+            </h3>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
